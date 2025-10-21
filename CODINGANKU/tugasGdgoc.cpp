91ab0de125bb0d18e10f9a80a32feb29cbd338d5
Super Fhmii <superfhmi09@gmail.com>
2025-10-21 11:51:10 +0800

    first commit

--- CODINGANKU/tugasGdgoc.cpp
+++ CODINGANKU/tugasGdgoc.cpp
@@ -2,15 +2,19 @@
 using namespace std;
 
 int main() {
-    int n, angka;
+    // 1. Variabel
+    int n; // variabel untuk menyimpan jumlah angka
+    int angka; // variabel untuk angka yang akan diperiksa
 
     cout << "Masukkan jumlah angka yang ingin diperiksa: ";
     cin >> n;
 
+    // 2. Perulangan (looping)
     for (int i = 1; i <= n; i++) {
         cout << "Masukkan angka ke-" << i << ": ";
         cin >> angka;
 
+        // 3. Percabangan (if-else)
         if (angka % 2 == 0) {
             cout << angka << " adalah bilangan genap." << endl;
         } else {
